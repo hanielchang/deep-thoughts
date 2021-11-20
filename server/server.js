305da2e11a9a0dd44b2245667b0e5f4aa9f5b988
hanielchang <haniellloyd@gmail.com>
2021-11-20 09:35:20 -0800

    saving work

---
+++ server/server.js
@@ -0,0 +1,44 @@
+// Creating a GraphQL API isn't much different than building a REST API with Express.js routes and controllers. 
+// Instead of creating a bunch of endpoints to handle all of the different types of requests, we consolidate 
+// them into one endpoint and provide more detailed information as to what the API is going to do for us.
+
+const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const startServer = async () => {
+  // create a new Apollo server and pass in our schema data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // context: authMiddleware
+  });
+
+  // Start the Apollo server
+  await server.start();
+
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+// Initialize the Apollo server
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
